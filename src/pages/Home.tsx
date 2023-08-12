0ffb04187c6f118737474e1f5ccd87c499bdba04
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-08-12 18:05:37 -0300

    Feat: Create and delete task Feat: Warning about user actions
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,46 @@
+import { useState } from 'react';
 import { Header } from '../components/Header';
-import { TodoTaskShow } from '../components/TodoTaskShow/TodoTaskShow';
+import { TodoTaskShow } from '../components/TodoTaskShow';
+import { TodoTaskListProps } from '../interfaces/interfaces';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function Home() {
+  const [task, setTask] = useState<string>('');
+
+  const [todoTaskList, setTodoTaskList] = useState<TodoTaskListProps[]>([]);
+
+  function createTask() {
+    if (task === '') {
+      toast.error(
+        'Oops! It looks like you left an empty field. Please fill in all required fields to continue.'
+      );
+    } else {
+      const generateTaskIDRandom = (num: number) =>
+        Math.floor(Math.random() * num);
+
+      const newTask = {
+        id: generateTaskIDRandom(99999999999),
+        taskName: task,
+      };
+
+      setTodoTaskList([...todoTaskList, newTask]);
+
+      toast.success('Task created with success!');
+    }
+  }
+
+  function deleteTask(DeleteTaskById: number): void {
+    setTodoTaskList(
+      todoTaskList.filter((taskName) => taskName.id !== DeleteTaskById)
+    );
+  }
+
   return (
     <>
+      <ToastContainer autoClose={3000} />
+
       <Header />
       <main className="w-4/5 my-0 mx-auto lg:w-1/2">
         <section className="mt-5 flex flex-col">
@@ -21,20 +58,23 @@ export function Home() {
               name="task"
               autoComplete="off"
               placeholder="Write your task"
+              value={task}
+              onChange={(event) => setTask(event.target.value)}
             />
           </div>
 
           <button
             className="bg-blue-600 text-blue-50 py-3 font-medium border-none rounded"
             type="submit"
+            onClick={createTask}
           >
             Add task
           </button>
         </section>
 
-        <div>
-          <TodoTaskShow />
-        </div>
+        {todoTaskList.map((task, key) => (
+          <TodoTaskShow key={key} task={task} deleteTask={deleteTask} />
+        ))}
       </main>
     </>
   );
