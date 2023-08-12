de3df70fa9008dbd3339699faf707fe2506ac831
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-08-12 18:04:39 -0300

    Fixe: Rename file
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/TodoTaskShow/index.tsx
@@ -0,0 +1,29 @@
+import { CheckCircle, Trash } from '@phosphor-icons/react';
+import { TodoTaskListProps } from '../../interfaces/interfaces';
+
+interface TodoTaskShowProps {
+  task: TodoTaskListProps;
+  deleteTask(DeleteTaskById: number): void;
+}
+
+export function TodoTaskShow({ task, deleteTask }: TodoTaskShowProps) {
+  return (
+    <div className="bg-blue-200 mt-4 py-4 px-2 flex items-center justify-between">
+      <div>
+        <p className="text-base font-medium text-blue-900">{task.taskName}</p>
+      </div>
+
+      <div className="flex items-center gap-1">
+        <span
+          className="cursor-pointer text-center text-sm"
+          onClick={() => deleteTask(task.id)}
+        >
+          <Trash size={16} />
+        </span>
+        <span className="cursor-pointer text-center text-sm">
+          <CheckCircle size={16} />
+        </span>
+      </div>
+    </div>
+  );
+}
