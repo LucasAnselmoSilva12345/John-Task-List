d2a2a42a0160236f82c2cfabdbe375e208208541
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-10-21 16:34:13 -0300

    Feat: ARIA rule propertie Feat: Styles of icon close modal
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,14 +14,17 @@ export function Modal({ isOpen, onClose, children }: ModalProps) {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black opacity-70 flex items-center justify-center">
-      <div className="p-4 bg-white rounded shadow-lg">
-        <button
-          className="absolute top-2 right-2 text-purple-200 transition-all duration-200 hover:opacity-50 cursor-pointer"
-          onClick={onClose}
-        >
-          <X weight="bold" fontSize={32} />
-        </button>
+      <div className="p-4 text-center bg-white rounded shadow-lg space-y-4">
         {children}
+        <button
+          className=" text-purple-800 transition-all duration-200 hover:opacity-50 cursor-pointer"
+          onClick={onClose}
+          aria-label="Close modal settings"
+          aria-hidden={!isOpen}
+          aria-controls="open-modal-settings"
+        >
+          <X weight="bold" fontSize={20} />
+        </button>
       </div>
     </div>
   );
