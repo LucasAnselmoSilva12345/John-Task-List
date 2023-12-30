ab3ca1f9ade8119edbe7a70e63297b5bbded8a4d
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-30 18:14:42 -0300

    fixe: Remove the ThemeToggle and Modal component.
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,4 @@
-import { useState } from 'react';
-import { LanguageSwitcher } from './LanguageSwitcher/LanguageSwitcher';
-import { Modal } from './Modal';
-import { Wrench } from '@phosphor-icons/react';
-import { ThemeToggle } from './ThemeToggle/ThemeToggle';
-import { useTranslation } from 'react-i18next';
-
 export function Header() {
-  const { t } = useTranslation();
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
     <header className="bg-violet-500 shadow dark:bg-neutral-900 ">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
@@ -19,25 +8,6 @@ export function Header() {
               John Task List
             </h1>
           </div>
-
-          <button
-            id="open-modal-settings"
-            className="text-purple-200 transition-all duration-200 hover:opacity-50 cursor-pointer"
-            onClick={() => setIsModalOpen(true)}
-            aria-label="Open modal settings"
-          >
-            <Wrench size={26} weight="bold" />
-          </button>
-          <Modal
-            title={t('jhlSetting')}
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-          >
-            <div className="w-full px-1 flex items-center justify-between">
-              <LanguageSwitcher />
-              <ThemeToggle />
-            </div>
-          </Modal>
         </div>
       </div>
     </header>
