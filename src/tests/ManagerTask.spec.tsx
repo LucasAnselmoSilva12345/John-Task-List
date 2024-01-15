17f007343ee8c93f7627c6111e5d05c565c35fa1
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-01-15 17:58:34 -0300

    test: Refactor Manager Task
    Issue: https://github.com/LucasAnselmoSilva12345/John-Task-List/issues/20
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/tests/ManagerTask.spec.tsx
+++ src/tests/ManagerTask.spec.tsx
@@ -2,21 +2,21 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { Home } from '../pages/Home';
 import { createTask } from './utils/createTask';
 
-describe('Manager Task ', () => {
-  it('should render input task field', () => {
+describe('Task Manager', () => {
+  beforeEach(() => {
     render(<Home />);
+  });
 
+  it('should render the task input field', () => {
     const inputTask = screen.getByRole('textbox', { name: 'Insert-New-Task' });
     expect(inputTask).toBeVisible();
   });
 
-  it('should create task', () => {
-    render(<Home />);
+  it('should create a task', () => {
     createTask('Go to the gym');
   });
 
-  it('should delete task', () => {
-    render(<Home />);
+  it('should delete a task', () => {
     const deleteTaskButton = screen.getAllByTestId('deleteTaskIcon');
     fireEvent.click(deleteTaskButton[0]);
 
@@ -27,8 +27,7 @@ describe('Manager Task ', () => {
     screen.queryByText(mockTaskName);
   });
 
-  it('should finished task', () => {
-    render(<Home />);
+  it('should mark a task as finished', () => {
     const deleteTaskButton = screen.getAllByTestId('deleteTaskIcon');
     fireEvent.click(deleteTaskButton[0]);
 
@@ -37,7 +36,7 @@ describe('Manager Task ', () => {
     const finishedTaskButton = screen.getAllByTestId('finishedTaskIcon');
     fireEvent.click(finishedTaskButton[0]);
 
-    screen.queryByText(mockTaskName);
+    expect(screen.queryByText(mockTaskName)).toBeNull();
   });
 });
 
