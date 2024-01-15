1708602e87c976be625a5aa1c7e3dea32d8e0c59
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-01-15 17:10:40 -0300

    fix: Refactor reference of import components
    Issue: https://github.com/LucasAnselmoSilva12345/John-Task-List/issues/20
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -1,13 +1,9 @@
-import { useTranslation } from 'react-i18next';
-
 export function Warning() {
-  const { t } = useTranslation();
-
   return (
     <div className="space-y-1">
       <div className="w-full h-1 bg-muted" />
       <span className="text-muted-foreground block text-center">
-        {t('withoutTask')}
+        No tasks created
       </span>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
-import { Header } from '@/components/Header';
-import { TodoTaskShow } from '@/components/TodoTaskShow/TodoTaskShow';
-import { TodoTaskListProps } from '@/types/TodoTaskListProps';
-import { Warning } from '@/components/Warning';
-import { Tasks } from '@/hook/localStorage/Tasks';
+import { Header } from '../components/Header';
+import { TodoTaskShow } from '../components/TodoTaskShow/TodoTaskShow';
+import { TodoTaskListProps } from '../types/TodoTaskListProps';
+import { Warning } from '../components/Warning';
+import { Tasks } from '../hook/localStorage/Tasks';
 
-import { todoMessages } from '@/toastMessages/toastMessages';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+import { todoMessages } from '../toastMessages/toastMessages';
+import { Label } from '../components/ui/label';
+import { Input } from '../components/ui/input';
+import { Button } from '../components/ui/button';
 import { toast } from 'sonner';
 
 interface TaskDataProps {
@@ -84,6 +84,7 @@ export function Home() {
               className="w-full bg-input border-border text-secondary-foreground"
               autoComplete="off"
               placeholder="It's necessary buy the new car"
+              aria-label="Insert-New-Task"
               value={newTaskName}
               onChange={handleTaskNameChange}
             />
